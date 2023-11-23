dbfd52b477c80beb84a2f03e5d2c3081dd832b31
sugara1995 <sugara1995530@gmail.com>
2023-11-23 20:37:09 +0100

    checking the guess in the word replace guess with blanks

--- Hang_man.py
+++ Hang_man.py
@@ -1,7 +1,35 @@
 import random
 
-words = ['bank','building','appartment']
+words = ['rank','building','appartment']
+
+display = []
 
 selected_word=words[random.randint(0,len(words)-1)]
 
+for char in selected_word:
+    display.append("_")
+
+
 print(selected_word)
+print(display)
+
+check_blanks = 1
+while(check_blanks==1):
+    
+    guess = input("enter your guess letter : ")
+
+    x=guess.lower()
+
+    i =0
+    for char in selected_word:
+        
+        if char == x:
+            display[i] = x
+            
+
+        i+=1  
+
+    print(display)
+
+    if "_" not in display:
+        check_blanks = 0
